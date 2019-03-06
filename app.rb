c7995d3774aaebf9c54dec0b369d842b952f0498
William J Teskey <willjteskey@gmail.com>
2019-03-06 21:10:11 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,3 +1,4 @@
+
 require_relative 'config/environment'
 require_relative 'models/text_analyzer.rb'
 
@@ -7,7 +8,7 @@ class App < Sinatra::Base
   end
 
   post '/' do
-    text_from_user = params[:user_text]
+    @analyzed_text = TextAnalyzer.new(params[:user_text])
 
     erb :results
   end
